ce660c8a355918194ca5783771730103566e20f3
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-04-18 23:52:27 +0200

    add import Container, Header, Footer components in the App component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import AddPost from './components/pages/AddPost/AddPost';
 import EditPost from './components/pages/EditPost/EditPost';
 import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/post/:id" element={<Post />} />
-    <Route path="/post/add" element={<AddPost />} />
-    <Route path="/post/edit/:id" element={<EditPost />} />
-    <Route path="/about" element={<About />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+  <Container>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post/:id" element={<Post />} />
+      <Route path="/post/add" element={<AddPost />} />
+      <Route path="/post/edit/:id" element={<EditPost />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </Container>
   );
 }
 
