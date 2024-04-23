f1081588e5eda27f25993040665a7871f222e90f
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-04-23 23:24:47 +0200

    add a single post page view

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -15,12 +15,12 @@ const Home = () => {
       {posts.map(post => (
         <Col key={post.id} lg={4} md={6} className="mt-3 mb-2">
           <Card className="p-3">
-            <Card.Title>{post.title}</Card.Title>
+            <Card.Title className="fs-3">{post.title}</Card.Title>
             <Card.Text  className="m-0"><span className={styles.headerBold}>Author:</span> {post.author}</Card.Text>
             <Card.Text><span className={styles.headerBold}>Published:</span> {post.publishedDate}</Card.Text>
             <Card.Text>{post.shortDescription}</Card.Text>
             <Link to={`/post/${post.id}`}>
-              <Button className={styles.btn} variant="primary" text-muted>Read more</Button>
+              <Button className={styles.btn} variant="primary">Read more</Button>
             </Link>
           </Card>
         </Col>
--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,6 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getElementById } from "../../../redux/postsRedux";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
+import styles from "./Post.module.scss";
+
 const Post = () => {
-  return(
-    <h1>Post</h1>
+
+  const { id } = useParams();
+
+  const posts = useSelector(state => getElementById(state, id));
+
+  if(!posts) {
+    return <Navigate to="/" />
+  } else return(
+    <Row>
+        <>
+          <Col lg={7} md={9} className="mt-3 mb-2 d-flex flex-column align-items-center">
+            <div className={`d-flex flex-column align-items-start ${styles.boxPosts}`}>
+              <Card.Title className="fs-2 mb-3">{posts.title}</Card.Title>
+              <Card.Text className="m-0"><span className={styles.headerBold}>Author:</span> {posts.author}</Card.Text>
+              <Card.Text><span className={styles.headerBold}>Published:</span> {posts.publishedDate}</Card.Text>
+              <Card.Text>{posts.content}</Card.Text>
+            </div>
+          </Col>
+          <Col lg={5} md={3} className="mt-3 mb-2">
+            <Link to={`/post/edit/${posts.id}`}>
+              <Button variant="outline-primary" className="me-2">Edit</Button>
+            </Link>
+            <Link>
+              <Button variant="outline-danger">Delete</Button>
+            </Link>
+          </Col>
+        </>
+    </Row>
   );
 };
 
