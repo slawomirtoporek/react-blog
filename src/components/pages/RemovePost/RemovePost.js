20d0e5f3e19560efb682e9a5787e357a5ed1d1f6
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-04-25 19:59:11 +0200

    add a popup to remove a post to the RemovePost component

--- src/components/pages/RemovePost/RemovePost.js
+++ src/components/pages/RemovePost/RemovePost.js
@@ -1,18 +1,43 @@
 import { useDispatch } from "react-redux";
 import { removePost } from "../../../redux/postsRedux";
-import { Button } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
 
 
 const RemovePost = ({ id }) => {
+
+  const [show, setShow] = useState(false);
+
+  const handleCloseModal = () => setShow(false);
+  const handleShowModal = () => setShow(true);
 
   const dispatch = useDispatch();
 
   const handleRemovePost = () => {
     dispatch(removePost(id));
+    handleCloseModal();
   };
 
   return(
-    <Button onClick={handleRemovePost} variant="outline-danger">Delete</Button>
+    <>
+      <Button onClick={handleShowModal} variant="outline-danger">Delete</Button>
+      <Modal show={show} onClick={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>The operation will completly remove this post from the app. Are you sure you want to do that?</p>  
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleRemovePost}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
